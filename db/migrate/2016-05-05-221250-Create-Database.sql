27038973734f15d7722de668e3309ebac426aafe
NomosNews <matt@nomos.io>
2016-07-06 15:00:06 +0000

    nn first commit

---
+++ db/migrate/2016-05-05-221250-Create-Database.sql
@@ -0,0 +1,3 @@
+/* Setup database for gohackernews */
+CREATE USER "gohackernews_server" WITH PASSWORD '5cd7dae4b10d2752';
+CREATE DATABASE "gohackernews_development" WITH OWNER "gohackernews_server";
